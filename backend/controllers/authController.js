6ac043c1ad6257dba6ecb584bc2df0edc4a7fa94
AnkushSonker <ankush.s@chalo.com>
2025-10-29 15:56:32 +0530

    login signup getprofile updateprofile controllers done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
-import User from "..models/User"
+import User from "../models/User.js"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
+
 
 // Generate jwt token
 const generateToken = (userId) => {
@@ -13,28 +14,125 @@ const generateToken = (userId) => {
 //@route    Post /api/auth/register
 //@access   Public
 const registerUser = async(req, res) => {
+    try {
+        const { name, email, password, profileImageUrl, adminInviteToken } = req.body;
 
+        // Check if user already exist
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // Determine user role: Admin if correct token is provided, otherwise member
+        let role = "member";
+        if (adminInviteToken && adminInviteToken == process.env.ADMIN_INVITE_TOKEN) {
+            role = "admin"
+        }
+
+        // Hash Password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create new user
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            profileImageUrl,
+            role,
+        });
+        // Return user data with JWT
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(user._id),
+        });
+
+    } catch (err) {
+        res.status(500).json({ message: "server Errorrrrrr", error: err.message });
+    }
 };
 
 //@desc     Login a User
 //@route    Post /api/auth/login
 //@access   Public
 const loginUser = async(req, res) => {
+    try {
+        const { email, password } = req.body;
 
+        const user = awaitUser.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        // Compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        // Return user data with JWT
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(user._id),
+        });
+    } catch (err) {
+        res.status(500).json({ message: "server Error", error: err.message });
+    }
 };
 
 //@desc     Get a User Profile
 //@route    GET /api/auth/profile
 //@access   Private(Requires JWT)
-const getUserProfile = async(req, req) => {
-
+const getUserProfile = async(req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ message: "server Error", error: err.message });
+    }
 };
 
 //@desc     Update a User
 //@route    PUT /api/auth/login
 //@access   Private (requires JWT)
 const updateUserProfile = async(req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
 
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(req.body.password, salt);
+        }
+
+        const updatedUser = await user.save();
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            role: updatedUser.role,
+            token: generateToken(updatedUser._id),
+        });
+    } catch (err) {
+        res.status(500).json({ message: "server Error", error: err.message });
+    }
 }
 
 export { registerUser, loginUser, getUserProfile, updateUserProfile };
