26b304055aa188f5ff2c6388a95f90dd8cd37007
AnkushSonker <ankush.s@chalo.com>
2025-10-30 09:04:41 +0530

    report routes added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js"
 import authRoutes from "./routes/authRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import taskRoutes from "./routes/taskRoutes.js"
+import reportRoutes from "./routes/reportRoutes.js"
 dotenv.config();
 
 const app = express();
@@ -29,7 +30,7 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
-//app.use("/api/reporst", reporstRoutes);
+app.use("/api/report", reportRoutes);
 //Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server runnign on port ${PORT}`));
